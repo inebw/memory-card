b541d807b0016727a3cb756c8c0fc6490474e1c7
Punit Yadav <punit965@gmail.com>
2025-11-23 19:07:11 +0530

    bug: all cards roatate on click now

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,93 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import getRandomArr from "./components/get-random-arr";
+import randomizeArray from "./components/randomizeArr";
 import GameBoard from "./components/game-board";
 import HasWon from "./components/has-won";
 
 function App() {
   const [hasWon, setHasWon] = useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [score, setScore] = useState(0);
+  const [dataVersion, setDataVersion] = useState(0);
+  const [selected, setSelected] = useState(new Set());
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://rickandmortyapi.com/api/character/";
+      const arr = getRandomArr();
+      try {
+        const response = await fetch(url + arr);
+        if (!response.ok) throw new Error("Server Error");
+        const result = await response.json();
+        setData(result);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [hasWon]);
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <div className="loader"></div>
+      </div>
+    );
+  }
+
+  function handleClick(e) {
+    let currId = 0;
+    setDataVersion(prevData => prevData + 1)
+    if (e.target.id) currId = e.target.id;
+    else currId = e.target.parentElement.id;
+    if (selected.has(currId)) {
+      setScore(0);
+      setSelected(new Set());
+    } else {
+      const newSet = new Set(selected);
+      newSet.add(currId);
+      setScore(score + 1);
+      setSelected(newSet);
+      const newData = randomizeArray(data);
+      setData(newData);
+    }
+  }
   const changeWonStatus = () => {
     setHasWon(!hasWon);
+    setScore(0);
   };
   return (
     <div className="app">
       <h1 className="logo">Rick and Morty Memory Game</h1>
       {hasWon ? (
-        <HasWon changeWonStatus={changeWonStatus} />
+        <HasWon
+          changeWonStatus={changeWonStatus}
+
+        />
       ) : (
-        <GameBoard changeWonStatus={changeWonStatus} />
+        data && (
+          <GameBoard
+            key={dataVersion}
+            changeWonStatus={changeWonStatus}
+            handleClick={handleClick}
+            score={score}
+            data={data}
+          />
+        )
       )}
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/game-board.jsx
+++ src/components/game-board.jsx
@@ -1,67 +1,19 @@
 import "./../styles/game-board.css";
-import getRandomArr from "./get-random-arr";
-import randomizeArray from "./randomizeArr";
+
 import { useState, useEffect } from "react";
 import Card from "./card";
 
-export default function GameBoard({ changeWonStatus }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [score, setScore] = useState(0);
+export default function GameBoard({
+  changeWonStatus,
+  handleClick,
+  score,
+  data,
+}) {
   const [highScore, setHighScore] = useState(0);
-  const [selected, setSelected] = useState(new Set());
 
   if (score > highScore) setHighScore(score);
   if (score == 12) changeWonStatus();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = "https://rickandmortyapi.com/api/character/";
-      const arr = getRandomArr();
-      try {
-        const response = await fetch(url + arr);
-        if (!response.ok) throw new Error("Server Error");
-        const result = await response.json();
-        setData(result);
-      } catch (e) {
-        setError(e);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
-  if (loading) {
-    return (
-      <div className="loading">
-        <div className="loader"></div>
-      </div>
-    );
-  }
-
-  function handleClick(e) {
-    let currId = 0;
-    if (e.target.id) currId = e.target.id;
-    else currId = e.target.parentElement.id;
-    if (selected.has(currId)) {
-      setScore(0);
-      setSelected(new Set());
-    } else {
-      const newSet = new Set(selected);
-      newSet.add(currId);
-      setScore(score + 1);
-      setSelected(newSet);
-      const newData = randomizeArray(data);
-      setData(null)
-      setData(newData);
-    }
-  }
   return (
     <div className="game-board">
       <div className="score-card">
